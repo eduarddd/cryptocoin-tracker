fca9fa99d7a362733d51174250738a54b0aabda5
eduardd <edusevilla90@gmail.com>
2017-12-05 21:02:59 +0100

    Implement simple activity with recyclerview that displays CryptoCoins

--- app/src/main/java/com/nightlydev/cryptocointracker/CryptoCoinsAdapter.kt
+++ app/src/main/java/com/nightlydev/cryptocointracker/CryptoCoinsAdapter.kt
@@ -4,18 +4,29 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.item_crypto_coin.view.*
 
 /**
  * Created by eduardo on 12/5/17.
  */
 class CryptoCoinsAdapter : RecyclerView.Adapter<CryptoCoinsAdapter.CryptoCoinViewHolder>() {
 
-    private var items: ArrayList<CryptoCoin> = ArrayList()
+    private var mItems: ArrayList<CryptoCoin>
 
-    override fun getItemCount(): Int = items.size
+    init {
+        mItems = ArrayList()
+    }
+
+    override fun getItemCount(): Int = mItems.size
 
     override fun onBindViewHolder(holder: CryptoCoinViewHolder, position: Int) {
-        holder.bindCryptoCoin(items[position])
+        val cryptoCoin = mItems[position]
+        holder.resetViews()
+
+        holder.rank.text = cryptoCoin.rank.toString()
+        holder.name.text = cryptoCoin.name
+        holder.priceUsd.text = holder.itemView.context.getString(R.string.price_usd_regex, cryptoCoin.price_usd.toString())
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CryptoCoinViewHolder {
@@ -26,11 +37,20 @@ class CryptoCoinsAdapter : RecyclerView.Adapter<CryptoCoinsAdapter.CryptoCoinVie
         return CryptoCoinViewHolder(view)
     }
 
+    fun add(cryptoCoin: CryptoCoin) {
+        mItems.add(cryptoCoin)
+        notifyItemInserted(mItems.indexOf(cryptoCoin))
+    }
 
     class CryptoCoinViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val rank: TextView = itemView.tv_rank
+        val name: TextView = itemView.tv_name
+        val priceUsd: TextView = itemView.tv_price_usd
 
-        fun bindCryptoCoin(cryptoCoin: CryptoCoin) {
-            TODO("Implement here code to bind a CryptoCoin")
+        fun resetViews() {
+            rank.text = ""
+            name.text = ""
+            priceUsd.text= ""
         }
     }
 }
--- app/src/main/java/com/nightlydev/cryptocointracker/OverviewActivity.kt
+++ app/src/main/java/com/nightlydev/cryptocointracker/OverviewActivity.kt
@@ -3,8 +3,7 @@ package com.nightlydev.cryptocointracker
 import android.app.Activity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_overview.*
 
 /**
  * Created by eduardo on 12/5/17.
@@ -18,11 +17,23 @@ class OverviewActivity : Activity() {
         setContentView(R.layout.activity_overview)
 
         initRecyclerView()
+        createFakeData()
     }
 
     private fun initRecyclerView() {
-        val recyclerView = findViewById<RecyclerView>(R.id.rv_crypto_coins_overview)
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-        recyclerView.adapter = mAdapter
+        with(rv_crypto_coins_overview) {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@OverviewActivity)
+            mAdapter = CryptoCoinsAdapter()
+            adapter = mAdapter
+        }
+    }
+
+
+    private fun createFakeData() {
+        for(i in 1..10) {
+            val fake = CryptoCoin("1", "Bitcoin", "BTC", 1, 11971.5)
+            mAdapter.add(fake)
+        }
     }
 }
