7f6a3b615dc1bfbd4d25f72fef7007dc16528917
eduardd <edusevilla90@gmail.com>
2018-01-16 22:38:42 +0100

    WIP: Add navigation bar

---
+++ app/src/main/java/com/nightlydev/cryptocointracker/ui/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.nightlydev.cryptocointracker.ui
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author edu (edusevilla90@gmail.com)
+ * @since 16-1-18
+ */
+class MainActivity : AppCompatActivity()
